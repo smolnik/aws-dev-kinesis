5d0358f97626b42a07ab2b2ef3dffda42b7ff9ee
Adam Smolnik <smolnik@wp.pl>
2016-02-01 00:35:07 +0100

    turning ShardIteratorType into LATEST

--- src/main/java/net/adamsmolnik/kinesis/api/PutGetApiExample.java
+++ src/main/java/net/adamsmolnik/kinesis/api/PutGetApiExample.java
@@ -3,6 +3,7 @@ package net.adamsmolnik.kinesis.api;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
@@ -10,6 +11,7 @@ import com.amazonaws.services.kinesis.model.GetRecordsRequest;
 import com.amazonaws.services.kinesis.model.GetRecordsResult;
 import com.amazonaws.services.kinesis.model.GetShardIteratorResult;
 import com.amazonaws.services.kinesis.model.PutRecordResult;
+import com.amazonaws.services.kinesis.model.Record;
 import com.amazonaws.services.kinesis.model.ShardIteratorType;
 
 /**
@@ -21,21 +23,31 @@ public class PutGetApiExample {
 
 	public static void main(String[] args) {
 		AmazonKinesis ak = new AmazonKinesisClient();
+		GetShardIteratorResult si = ak.getShardIterator("kin-test", "000000000001", ShardIteratorType.LATEST.toString());
+		GetRecordsRequest getReq = new GetRecordsRequest().withShardIterator(si.getShardIterator()).withLimit(1000);
+
 		PutRecordResult putRes1 = ak.putRecord("kin-test",
 				ByteBuffer.wrap(("Message 1. for pk1 at " + LocalDateTime.now()).getBytes(StandardCharsets.UTF_8)), "pk1");
 		System.out.println(putRes1);
 		PutRecordResult putRes2 = ak.putRecord("kin-test",
 				ByteBuffer.wrap(("Message 2. for pk1 at " + LocalDateTime.now()).getBytes(StandardCharsets.UTF_8)), "pk1");
 		System.out.println(putRes2);
-		GetShardIteratorResult si = ak.getShardIterator("kin-test", "000000000001", ShardIteratorType.TRIM_HORIZON.toString());
-		GetRecordsRequest getReq = new GetRecordsRequest().withShardIterator(si.getShardIterator()).withLimit(1000);
-		GetRecordsResult getRes = ak.getRecords(getReq);
-		getRes.getRecords().forEach(r -> {
-			ByteBuffer bb = r.getData();
-			byte[] arr = new byte[bb.limit()];
-			bb.get(arr, 0, arr.length);
-			System.out.println(Thread.currentThread() + ": " + new String(arr, StandardCharsets.UTF_8));
-		});
+		while (true) {
+			GetRecordsResult getRes = ak.getRecords(getReq);
+			List<Record> recs = getRes.getRecords();
+			if (!recs.isEmpty()) {
+				getRes.getRecords().forEach(r -> {
+					ByteBuffer bb = r.getData();
+					byte[] arr = new byte[bb.limit()];
+					bb.get(arr, 0, arr.length);
+					System.out.println(Thread.currentThread() + ": " + new String(arr, StandardCharsets.UTF_8));
+				});
+				break;
+			}
+			String next = getRes.getNextShardIterator();
+			System.out.println("empty, next: " + next);
+			getReq = new GetRecordsRequest().withShardIterator(next).withLimit(10000);
+		}
 	}
 
 }
